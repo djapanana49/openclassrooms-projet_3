518659153ad88c0f543a3ae49e167800fd1bcd64
SYLVIE <SYLVIE@SYLVIE-PC>
2018-09-04 23:17:50 +0200

    page_404

--- view/page_404.php
+++ view/page_404.php
@@ -1,5 +1,7 @@
-<h1 class="display-1">404</h1>
+<div class="col-lg-8 col-md-10 mx-auto">
+<h1 class="display-1">Page 404</h1>
 <p class="lead">Page non trouv&eacute;e. Vous pouvez faire un
             <a href="javascript:history.back()"class="btn btn-info" role="button">retour</a>
             sur la page précédente, ou 
             <a href="index.php?action=listPostsView" class="btn btn-info" role="button">retourner sur la page d'accueil</a>.</p>
+</div>
